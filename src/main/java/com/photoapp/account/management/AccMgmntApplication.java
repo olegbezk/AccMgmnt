41ed08b7cc29747c295fd8218c23119f571e0f91
Oleg Bezkorovaynyi <oleg.bezkorovajniy@gmail.com>
2021-06-08 11:44:06 +0300

    initial

---
+++ src/main/java/com/photoapp/account/management/AccMgmntApplication.java
@@ -0,0 +1,13 @@
+package com.photoapp.account.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccMgmntApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccMgmntApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/photoapp/account/management/AccMgmntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photoapp.account.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccMgmntApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
